b81c033f0cf0766735421773d90e216dbf3488bf
Oded Spitzer <odedspitzer@gmail.com>
2017-12-24 18:29:35 +0200

    some small changes Oded

---
+++ src/components/HomePage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>TriVue</h1>
+    <button>Play</button>
+    <button>Log In</button>
+    <button>Sign Up</button>
+    <quest></quest>
+  </div>
+</template>
+
+<script>
+import Quest from "./Quest.vue";
+
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      
+    }
+  },
+  components: Quest
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
